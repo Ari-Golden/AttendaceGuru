2a35e24fbfee53222a101744310c1d15aed63657
Ari-Golden <goldennationallegacy@gmail.com>
2025-08-13 15:08:59 +0700

    edit dashboard.blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,7 +47,10 @@
             </div>
 
             <!-- Card 4: Belum Absen -->
-            <div class="bg-white p-4 rounded-lg shadow-md flex items-center space-x-3 w-1/4">
+            @if (Auth::check() && Auth::user()->hasRole('admin'))
+            <a href="{{ route('absensi.non-attending') }}" class="w-1/4">
+            @endif
+            <div class="bg-white p-4 rounded-lg shadow-md flex items-center space-x-3 w-full">
                 <div class="bg-yellow-500 p-3 rounded-full">
                     <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
@@ -58,6 +61,9 @@
                     <p class="text-2xl font-bold text-gray-900">{{ $totalBelumAbsen }}</p>
                 </div>
             </div>
+            @if (Auth::check() && Auth::user()->hasRole('admin'))
+            </a>
+            @endif
         </div>
 
         <!-- Filter dan Pencarian -->
